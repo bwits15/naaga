c390b49b0e7657f5d82e6bd44b420d06b62e3a8c
Somyeong Kim <bwits15@users.noreply.github.com>
2025-12-31 02:43:09 +0900

    initial commit for naaga website

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/activities")
+def activities():
+    return render_template("activities.html")
+
+@app.route("/vision")
+def vision():
+    return render_template("vision.html")
+
+@app.route("/members")
+def members():
+    return render_template("members.html")
+
+@app.route("/recruit")
+def recruit():
+    return render_template("recruit.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
